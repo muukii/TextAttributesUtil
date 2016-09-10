f267337e1c9d7be5f4c70407c27102532a9ef6c0
muukii <m@muukii.me>
2016-09-10 15:09:34 +0900

    🤔WIP [ci skip]

---
+++ TextAttributesUtil/TextAttributesUtil.h
@@ -0,0 +1,19 @@
+//
+//  TextAttributesUtil.h
+//  TextAttributesUtil
+//
+//  Created by muukii on 9/10/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextAttributesUtil.
+FOUNDATION_EXPORT double TextAttributesUtilVersionNumber;
+
+//! Project version string for TextAttributesUtil.
+FOUNDATION_EXPORT const unsigned char TextAttributesUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextAttributesUtil/PublicHeader.h>
+
+
